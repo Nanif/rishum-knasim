09c046d138c2dd9aefbaf34dc6cf8b2f6ddba56f
משפחת פישביין <neomi2152@gmail.com>
2019-08-05 03:15:42 +0300

    first

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MailService } from './mail.service';
+ 
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from:'"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new HandlebarsAdapter(), // or new PugAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [AppService, MailService],
+  controllers: [AppController]
+})
+export class AppModule {}
