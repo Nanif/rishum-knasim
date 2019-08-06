fb2cb4f311f157090a414a33bbef5f6a284783ee
משפחת פישביין <neomi2152@gmail.com>
2019-08-07 02:36:06 +0300

    add service mail

---
+++ src/mail.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nest-modules/mailer';
+ 
+@Injectable()
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+    
+  public example(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'neomi2152@gmail.com', // sender address
+        from: 'neomi2152@gmail.com', // list of receivers
+        subject: 'Testing Nest MailerModule ✔', // Subject line
+        text: 'welcome', // plaintext body
+        html: '<b>welcome</b>', // HTML body content
+      })
+      .then(() => {})
+      .catch(() => {});
+  }
+  
+  public example2(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: 'welcome', // The `.pug` or `.hbs` extension is appended automatically.
+        context: {  // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then(() => {})
+      .catch(() => {});
+  }
+  
+  public example3(): void {
+    this
+      .mailerService
+      .sendMail({
+        to: 'test@nestjs.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: __dirname + '/welcome', // The `.pug` or `.hbs` extension is appended automatically.
+        context: {  // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe',
+        },
+      })
+      .then(() => {})
+      .catch(() => {});
+  }
+}
