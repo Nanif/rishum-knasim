b21c1251227f0688cd3336941726b30af5558b83
משפחת פישביין <neomi2152@gmail.com>
2019-08-13 00:00:50 +0300

    change pdf lib to pdf kit

--- src/receipt.service.ts
+++ src/receipt.service.ts
@@ -1,7 +1,6 @@
 import {Injectable } from "@nestjs/common";
-import {PDFDocument, StandardFonts, rgb, degrees} from 'pdf-lib'
-let fs = require('fs');
-
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 @Injectable()
 export class ReceiptService {
 
@@ -12,50 +11,54 @@ export class ReceiptService {
 
     public async pdf() {
 
-        const pdfDoc = await PDFDocument.create()
-// Embed the Times Roman font
-        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
 
-// Add a blank page to the document
-        const page = pdfDoc.addPage()
+// Create a document
+        const doc = new PDFDocument();
 
-// Get the width and height of the page
-        const {width, height} = page.getSize()
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+        doc.pipe(fs.createWriteStream('output.pdf'));
 
-// Draw a string of text toward the top of the page
-        const fontSize = 30
-        // const jpgUrl = 'http://shmura.org/wp-content/uploads/2019/06/unnamed.jpg'
-        // const jpgImageBytes = await this.http.get(jpgUrl).subscribe(res => res.arrayBuffer())
+// Embed a font, set the font size, and render some text
+//         doc.font('fonts/PalatinoBold.ttf')
+//             .fontSize(25)
+//             .text('Some text with an embedded font!', 100, 100);
 
-        const jpgImage = await pdfDoc.embedJpg('shmura.jpg')
-        const jpgDims = jpgImage.scale(0.5)
+// Add an image, constrain it to a given size, and center it vertically and horizontally
+//         doc.image('path/to/image.png', {
+//             fit: [250, 300],
+//             align: 'center',
+//             valign: 'center'
+//         });
 
+// Add another page
+//         doc.addPage()
+//             .fontSize(25)
+//             .text('Here is some vector graphics...', 100, 100);
 
-        // page.drawImage(jpgImage, {
-        //     x: 25,
-        //     y: 25,
-        //     width: jpgDims.width,
-        //     height: jpgDims.height,
-        // });
-        page.drawText('Creating PDFs in JavaScript is awesome!', {
-            x: 50,
-            y: height - 4 * fontSize,
-            size: fontSize,
-            font: timesRomanFont,
-            color: rgb(0, 0.53, 0.71),
-        })
+// Draw a triangle
+        doc.save()
+            .moveTo(100, 150)
+            .lineTo(100, 250)
+            .lineTo(200, 250)
+            .fill("#FF3300");
 
-// Serialize the PDFDocument to bytes (a Uint8Array)
-        const pdfBytes = await pdfDoc.save()
+// Apply some transforms and render an SVG path with the 'even-odd' fill rule
+        doc.scale(0.6)
+            .translate(470, -380)
+            .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+            .fill('red', 'even-odd')
+            .restore();
 
-        fs.appendFile(`${this.filePath}`, pdfBytes, function (err) {
-            if (err) {
-                throw(err);
-            } else {
-            }
-        });
+// Add some text with annotations
+        doc.addPage()
+            .fillColor("blue")
+            .text('Here is a link!', 100, 100)
+            .underline(100, 100, 160, 27, {color: "#0000FF"})
+            .link(100, 100, 160, 27, 'http://google.com/');
 
-        console.log(pdfBytes);
+// Finalize PDF file
+        doc.end();
     }
 
     public getFilePath(): string {
