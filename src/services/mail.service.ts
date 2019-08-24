1a9f60fc8df9a6afb1590dadacf05287ce46a5da
פישביין <neomi2152@gmail.com>
2019-08-24 23:59:38 +0300

    email

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -14,11 +14,20 @@ export class MailService {
 
     public async sendApplicationMail(data) {
         const mailOptions: ISendMailOptions = {
-            to: 'neomi2152@gmail.com', // sender address
+            to: data.Email, // sender address
             from: 'neomi2152@gmail.com', // list of receivers
-            subject: 'Testing Nest MailerModule ✔', // Subject line
-            text: 'welcome', // plaintext body
-            html: '<b>welcome</b>', // HTML body content
+            subject: 'כרטיס כניסה לכנס יהיו לרצון', // Subject line
+            text: `${data.PrivateName} `, // plaintext body
+            html: `<b>נעמי יקרה</b>
+                    <br>
+                    <p>תודה שרצית להיות חלק,</p>
+                    <p>שמחות שבחרת לפסוע איתנו במסלול העולה-</p>
+                    <p>ויהי רצון שיעלה לרצון לפניו יתברך....</p>
+                    <p>מצורפת קבלה עבור תשלום דמי ההשתתפות בכנס.</p>
+                    <p>הקבלה היא כרטיס הכניסה שלך</p>
+                    <p>נא הדפיסי והציגי אותה בכניסה לאולם.</p>
+                    <p>מחכות לראותך,</p>
+                    <p>צוות השמורה</p>`, // HTML body content
             attachments: [{
                 filename: 'קבלה.pdf',
                 path: `${data.ArmyID}.pdf`
@@ -34,7 +43,7 @@ export class MailService {
                 console.log('removed!')
             })
         }).catch(err => {
-            console.log(err);
+            console.log('sendMail', err);
         });
     }
 }
