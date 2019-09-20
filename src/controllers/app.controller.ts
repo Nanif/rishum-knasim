72aa8e6f7438531c30c1d396d23edd0a69a111ad
פישביין <neomi2152@gmail.com>
2019-09-20 03:31:59 +0300

    Send card to kenes only -> bug

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,47 +1,51 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
-import {AppService} from '../services/app.service';
-import {MailService} from "../services/mail.service";
-import {PdfFileService} from "../services/pdfFile.service";
-import {MongoDBService} from '../services/mongoDB.service';
-
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from '../services/app.service';
+import { MailService } from '../services/mail.service';
+import { PdfFileService } from '../services/pdfFile.service';
+import { MongoDBService } from '../services/mongoDB.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService,
-                private readonly mailService: MailService,
-                private readonly pdfFileService: PdfFileService,
-                private readonly mongoDBService: MongoDBService
-    ) {
+  constructor(private readonly appService: AppService,
+              private readonly mailService: MailService,
+              private readonly pdfFileService: PdfFileService,
+              private readonly mongoDBService: MongoDBService,
+  ) {
+  }
+
+  @Post('sendReceipt')
+  SendReceipt(@Body() data) {
+    // send data to knasim@gmail.com mail
+    this.mailService.sendDataForSaveIt(data).then(() => {
+        // tslint:disable-next-line:no-console
+      console.log('data sent....');
+    }).catch((error) => {
+        // tslint:disable-next-line:no-console
+      console.log('data didnt saved', error, data);
+    });
+
+    // send card to user email
+    let isRishumKnasim = false;
+    if (data.Groupe === 'כנס אלול מודיעין עילית' || data.Groupe === 'כנס אלול ירושלים' || data.Groupe === 'כנס אלול בני ברק') {
+       isRishumKnasim = true;
     }
 
-    @Post('sendReceipt')
-    SendReceipt(@Body() data) {
-        // send data to knasim@gmail.com mail
-        this.mailService.sendDataForSaveIt(data).then(() => {
-            console.log('data sent....')
-        }).catch((error) => {
-            console.log('data didnt saved', error, data)
-        });
-
-        // send card to user email
-        let isRishumKnasim = true;
-        if (data.Groupe) {
-            isRishumKnasim = data.Groupe === 'שמועה' ||  data.Groupe === 'תרומה לשמורה'? false : true
-        }
-        if (isRishumKnasim) {
-            this.mailService.sendApplicationMail(data).then(() => {
-                console.log('card sent')
-            }).catch((error) => {
-                console.log('card didnt sent', error, data)
-            });
-        }
-
-        //save data in mongodb
-        // @ts-ignore
-        // this.mongoDBService.saveDataInDb(data).then((res) => {
-        //     console.log('res', res)
-        // }).catch((error) => {
-        //     console.log('error', error);
-        // });
+    if (isRishumKnasim) {
+      this.mailService.sendApplicationMail(data).then(() => {
+          // tslint:disable-next-line:no-console
+        console.log('card sent');
+      }).catch((error) => {
+          // tslint:disable-next-line:no-console
+        console.log('card didnt sent', error, data);
+      });
     }
+
+    //save data in mongodb
+    // @ts-ignore
+    // this.mongoDBService.saveDataInDb(data).then((res) => {
+    //     console.log('res', res)
+    // }).catch((error) => {
+    //     console.log('error', error);
+    // });
+  }
 }
